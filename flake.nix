fdf356983afc85b06ea6ce527ada2476b6f95194
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2023-11-12 09:04:11 -0800

    Inital commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nix-flake-based Hugo envionment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    dagger = {
+      url = "github:dagger/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, dagger, ... }:
+    flake-utils.lib.eachDefaultSystem ( system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          dagger.packages.${system}.dagger
+        ];
+      };
+    }
+    );
+}
